4ea4ff6d2308854deac961dbdce274d5a2de9606
Cezar Augusto Mezzalira <cezar.mezzalira@gmail.com>
2019-06-05 17:03:06 -0300

    Desafio Schedule

--- src/app/controllers/DashboardController.js
+++ src/app/controllers/DashboardController.js
@@ -1,9 +1,36 @@
-const { User } = require('../models')
+const { User, Appointment } = require('../models')
+
+const { Op } = require('sequelize')
+const moment = require('moment')
 
 class DashboardController {
   async index (req, res) {
-    const providers = await User.findAll({ where: { provider: true } })
-    return res.render('dashboard', { providers })
+    const { user } = req.session
+    if (!user.provider) {
+      const providers = await User.findAll({ where: { provider: true } })
+      return res.render('dashboard', { providers })
+    } else {
+      const date = moment()
+      const appointments = await Appointment.findAll({
+        where: {
+          provider_id: user.id,
+          date: {
+            [Op.between]: [
+              date.startOf('day').format(),
+              date.endOf('day').format()
+            ]
+          }
+        },
+        include: [
+          {
+            model: User,
+            as: 'user'
+          }
+        ]
+      })
+      // console.log(appointments)
+      return res.render('dashboard', { appointments })
+    }
   }
 }
 module.exports = new DashboardController()
